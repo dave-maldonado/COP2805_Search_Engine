31a388b608c30295f169cb6a057f8ed6f2d2abe3
David Maldonado <david.m.maldonado@gmail.com>
2013-02-05 15:26:55 -0500

    initial commit of GUI class

---
+++ SearchGUI.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
+/*
+ * GUI for Search Application
+ */
+public class SearchGUI {
+
+    public static void main(String[] args) {
+        new SearchGUI();
+    }
+    
+    public SearchGUI() {
+        JFrame frame = new JFrame();
+        frame.setTitle("Search and ye shall find");
+        frame.setSize(500,400);
+        
+        Container pane = frame.getContentPane();
+        pane.setLayout(new GridLayout(3,2));
+        
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        frame.setVisible(
+
