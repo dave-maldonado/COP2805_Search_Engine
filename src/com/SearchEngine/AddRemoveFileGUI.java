a996c29909b2f6b3508cbdf57e837530b36db50b
Andrew Medeiros <amedeiros@clarityservices.com>
2013-02-19 20:15:13 -0500

    Working on adding the table

--- src/com/SearchEngine/AddRemoveFileGUI.java
+++ src/com/SearchEngine/AddRemoveFileGUI.java
@@ -5,19 +5,26 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JTable;
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
 import javax.swing.JSplitPane;
 import javax.swing.ListSelectionModel;
+import javax.swing.JScrollPane;
 
 public class AddRemoveFileGUI {
 
 	private JFrame frmAddOrRemove;
-	private JTable table;
-	private JSplitPane splitPane;
+	private JSplitPane bottomSplitPane;
 	private JButton btnAddFile;
 	private JButton btnRemoveFile;
+	private JScrollPane scrollPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -51,22 +58,37 @@ public class AddRemoveFileGUI {
 		frmAddOrRemove.setBounds(100, 100, 450, 300);
 		frmAddOrRemove.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
-		table = new JTable();
-		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		frmAddOrRemove.getContentPane().add(table, BorderLayout.CENTER);
+		scrollPane = new JScrollPane();
+		frmAddOrRemove.getContentPane().add(scrollPane, BorderLayout.CENTER);
+		
+		String[] columnNames = { "Filename", "Status" };
+		String[][] data = {};
+		
+		table = new JTable(data, columnNames);
+		
+		scrollPane.add(table);
 		
 		JLabel lblAddOrRemove = new JLabel("Add Or Remove Files From The Index");
 		frmAddOrRemove.getContentPane().add(lblAddOrRemove, BorderLayout.NORTH);
 		
-		splitPane = new JSplitPane();
-		splitPane.setResizeWeight(0.5);
-		frmAddOrRemove.getContentPane().add(splitPane, BorderLayout.SOUTH);
+		bottomSplitPane = new JSplitPane();
+		bottomSplitPane.setResizeWeight(0.5);
+		frmAddOrRemove.getContentPane().add(bottomSplitPane, BorderLayout.SOUTH);
 		
 		btnAddFile = new JButton("Add File");
-		splitPane.setLeftComponent(btnAddFile);
+		bottomSplitPane.setLeftComponent(btnAddFile);
+		btnAddFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				final JFileChooser fc = new JFileChooser();
+				fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+				fc.showOpenDialog(null);
+				File selectedFile = fc.getSelectedFile();
+			}
+		});
 		
 		btnRemoveFile = new JButton("Remove File");
-		splitPane.setRightComponent(btnRemoveFile);
+		bottomSplitPane.setRightComponent(btnRemoveFile);
+		
 	}
 
 }
