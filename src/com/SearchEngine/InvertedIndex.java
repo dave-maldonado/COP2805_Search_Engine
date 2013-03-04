46120e8f873c932b9fef7be08e2be57bba4ee98d
David Maldonado <david.m.maldonado@gmail.com>
2013-03-04 09:09:53 -0500

    changed data structure and added Pair inner class

--- src/com/SearchEngine/InvertedIndex.java
+++ src/com/SearchEngine/InvertedIndex.java
@@ -1,55 +1,92 @@
 package com.SearchEngine;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 /**
  * Builds an Inverted Index with supplied String list of files with entries
- * in following format: { filepath , status, last accessed }, see FileIndex class
+ * in following format: { file path , status, last accessed }, see FileIndex class
  * @author dave maldonado 2013
  */
 public class InvertedIndex {
-	
-	
+		
 	private int size;
+	private int docID = 1;
 	final static String DELIMITER = ",";
 	
 	// list of files to be parsed
-	private List<String> filePaths = new ArrayList<String>();
+	private List<String> files = new ArrayList<String>();
 	
-	// data structure for inverted index 
-	// outer HashMap - keys are parsed words from docs, values are inner HashMap
-	// inner HashMap - keys are document names, values are position in document
-	private HashMap<String,HashMap<String,Integer>> foo = new HashMap<String,HashMap<String,Integer>>();
-	
+	// data structure for inverted index:
+	// keys are parsed words from docs, values are an ArrayList of Pair Objects 
+	private HashMap<String,ArrayList> invertInd = new HashMap<String,ArrayList>();
+
+	// private inner class for Pair Objects, pairs will consist of {docID, position of word in doc}
+	private class Pair<L,R> {
+		
+		private final L left;
+		private final R right;
+		
+		public Pair(L left, R right) {
+			this.left = left;
+			this.right = right;
+		}
+		
+		public L getLeft() { 
+			return left; 
+		}
+		
+		public R getRight() { 
+			return right; 
+		}
+		
+		@Override
+		public int hashCode() {
+			return left.hashCode() ^ right.hashCode(); 
+		}
+		
+		@Override
+		public boolean equals(Object o) {
+			if (o == null) return false;
+			if (!(o instanceof Pair)) return false;
+			Pair pairo = (Pair) o;
+			return this.left.equals(pairo.getLeft()) &&
+				   this.right.equals(pairo.getRight());
+		}
+	}
 	
 	/**
 	 * Constructor that calls telescoped constructor with additional size argument
 	 * @param file
+	 * @throws FileNotFoundException 
 	 */
-	public InvertedIndex(List<String> files) {
-		new InvertedIndex (files, files.size());
+	public InvertedIndex(List<String> input) {
+		new InvertedIndex (input, input.size());
 	}
 	
 	/**
 	 * Constructor that reads in String list of files and size of list then parses files
 	 * and builds inverted index
 	 * @param file, size
+	 * @throws FileNotFoundException 
 	 */
-	private InvertedIndex(List<String> files, int size) {
+	private InvertedIndex(List<String> input, int size) {
 		
-		// copies file path info from input list to filePaths,
-		// NOTE TO SELF: make sure this isn't a memory hog (looks kinda bad with all the created arrays)
-		for (String s : files) {
+		// creates String ArrayList w/ format {docID (number), file path}
+		for (String s : input) {
 			String[] tokens = s.split(DELIMITER);
-			filePaths.add(tokens[0]);
+			files.add(docID + DELIMITER + tokens[0]);
+			docID++;
 		}
+		
+		// populates HashMaps
+		
 		
 		//for testing
 		System.out.println("size of passed array:" + size);
-		System.out.println("contents of passed array   :" + files);
-		System.out.println("contents of processed array:" + filePaths);
+		System.out.println("contents of passed list:   " + input);
+		System.out.println("contents of processed list:" + files);
 	}
+	
 }
